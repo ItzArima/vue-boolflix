de1f45b512af119a4864af2db5e783ad3233187d
ItzArima <skaypaprilia@gmail.com>
2021-11-18 12:01:13 +0100

    base structure

---
+++ src/components/siteHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>this is the site header</h1>
+</template>
+
+<script>
+export default {
+    name : 'siteHeader'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/siteMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>this is the site main</h1>
+</template>
+
+<script>
+export default {
+    name :'siteMain'
+}
+</script>
+
+<style>
+
+</style>
